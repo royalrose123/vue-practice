81f5b211fa23b15f5488c02ae2e8e4dd19fff946
Royal Chen <royal.chen@cloud-interactive.com>
2020-11-24 17:30:58 +0800

    Add todolist page

---
+++ components/Todo.vue
@@ -0,0 +1,58 @@
+<template>
+  <div :class="['todo', todo.isCompleted? 'success':'danger']" >
+    <p class='title'>{{todo.title}}</p>
+    <input class='checkbox' type='checkbox' v-model='todo.isCompleted'/>
+    <p class='delete' @click='deleteTodo'>X</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Button',
+  props: ['todo','index'],
+  methods:{
+    deleteTodo(){
+      this.$emit('delete-todo',this.index)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.todo {
+  width: 100%;
+  height: 48px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid black;
+  margin: -1px 0px;
+  padding: 8px;
+  padding-right: 16px;
+}
+
+.success {
+  border-left: 4px solid green;
+}
+
+.danger {
+  border-left: 4px solid red;
+}
+
+.title {
+  width: 400px;
+}
+
+.checkbox {
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+}
+
+.delete {
+  cursor: pointer;
+}
+
+</style>
---
+++ pages/todolist.vue
@@ -0,0 +1,60 @@
+
+<template>
+  <div class='wrapper'>
+    <h1 class='title'>Todo List</h1>
+    <div class='todolist'>
+      <Todo class='todo' v-for='(todo, index) in todos' :key="todo.id" :todo='todo' :index='index' @delete-todo="handleDeleteTodo"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from '../components/Todo'
+
+export default {
+  data(){
+    return {
+      todos : [
+        {title: '吃飯', isCompleted: false},
+        {title: '出門', isCompleted: false},
+        {title: '買菜', isCompleted: true},
+        {title: '剪頭髮', isCompleted: false},
+        {title: '運動', isCompleted: false},
+      ]
+    }
+  },
+  methods:{
+    handleDeleteTodo(selectItemIndex){
+      this.todos = this.todos.filter((item,index) =>  index !== selectItemIndex)
+    }
+  },
+  components: {
+    Todo,
+  }
+}
+</script>
+
+
+
+<style scoped>
+  .wrapper{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .title{
+    margin: 16px;
+  }
+
+  .todolist{
+    width: 480px;
+    height: 600px;
+    border: 1px solid black;
+    overflow-y: scroll;
+    margin: 0px -1px;
+  }
+</style>
